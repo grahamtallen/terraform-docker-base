77b5fcb1fbbecfd2b9a5e42c11d98941d3510bc4
grahamtallen <graham.develops@gmail.com>
2020-10-01 22:45:50 -0700

    (almost) completely working cloud init

--- playground/servers.tf
+++ playground/servers.tf
@@ -1,13 +1,17 @@
+data "template_file" "user_data" {
+	template = file("../scripts/add-pubkeys.yml")
+}
+
 resource "aws_instance" "test-ec2-instance" {
 	ami = var.ami_id
 	instance_type = "t2.micro"
-	key_name = var.ami_key_pair_name
 	security_groups = [aws_security_group.ingress-all-test.id]
+	subnet_id = aws_subnet.subnet-uno.id
+
+	user_data = data.template_file.user_data.rendered
 	
 	tags = {
 		Name = var.ami_name
 	}
-
-	subnet_id = aws_subnet.subnet-uno.id
 
 }
